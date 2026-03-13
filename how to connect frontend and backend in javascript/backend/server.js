5d9a42f88a7696c2fd6e80a2aa8012946a3c6fce
Umang Raval <umangraval0365@gmail.com>
2026-03-13 11:34:42 +0530

    learning about proxy

---
+++ how to connect frontend and backend in javascript/backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("server is ready");
+// });
+
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A joke",
+      content: "This is a joke 1",
+    },
+    {
+      id: 2,
+      title: "A joke",
+      content: "This is a joke 2",
+    },
+    {
+      id: 3,
+      title: "A joke",
+      content: "This is a joke 3",
+    },
+    {
+      id: 4,
+      title: "A joke",
+      content: "This is a joke 4",
+    },
+    {
+      id: 5,
+      title: "A joke",
+      content: "This is a joke 5",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
