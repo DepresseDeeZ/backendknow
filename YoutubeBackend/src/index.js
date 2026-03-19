9b6cef844d4d0929e90f1e0e68e7bf975d921449
Umang Raval <umangraval0365@gmail.com>
2026-03-19 23:36:28 +0530

    07 How to connect database in MERN with debugging

---
+++ YoutubeBackend/src/index.js
@@ -0,0 +1,27 @@
+// require("dotenv").config({ path: [".env.local", ".env"] });
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+/* First Approach
+const app = express();
+// created a ify
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Err:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
