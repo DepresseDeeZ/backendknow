df7ec8418a704aabf00e7fb3cd096f53015ca2b3
Umang Raval <umangraval0365@gmail.com>
2026-03-20 15:24:12 +0530

    Custom api response and error handling

---
+++ YoutubeBackend/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// using promise
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asynchandler = () => {};
+// const asynchandler = (fn) => () => {};
+// const asynchandler = (fn) => async () => {};
+
+// try catch
+// const asynchandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
